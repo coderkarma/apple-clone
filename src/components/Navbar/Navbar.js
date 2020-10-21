27b75b651a29dadf3249a7f6a2bb57d5cd3b537f
Karma Drukpa <karmadrukpa11@gmail.com>
2020-10-20 23:57:52 -0400

    Add image and navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+	return (
+		<div className='navbar'>
+			<div className='nabvar__content'>
+				<ul className='navbar__lists'>
+					<li>Logo</li>
+					<li>Mac</li>
+					<li>Ipad</li>
+					<li>iPhone</li>
+					<li>Watch</li>
+					<li>TV</li>
+					<li>Music</li>
+					<li>Support</li>
+					<li>Search</li>
+					<li>Bag</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
